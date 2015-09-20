128931c8a2d02d5b7bd5ec91afc068deab2205e3
Nicole Cappadocia-Assaly <nicolecappadocia@gmail.com>
2015-09-19 22:39:30 -0400

    Set up data structures to keep the current states. Set up urls and views to transfer state to front end AJAX

--- elpolitico/elpolitico/views.py
+++ elpolitico/elpolitico/views.py
@@ -3,6 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from elpolitico.settings import STATICFILES_DIRS
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect, HttpResponseNotFound, HttpResponseBadRequest
+from State import States
+import State
+import json
 
 def home(request):
     print(STATICFILES_DIRS)
@@ -10,3 +13,19 @@ def home(request):
         request,
         'index.html'
     )
+
+def party_check(request, party=None):
+    if request.method == "POST":
+        for state in State.STATES:
+            data = States.getState(state)
+            data = json.dumps(data)
+            return HttpResponse(data)
+    return HttpResponseRedirect('/')
+
+def new_points_check(request):
+    # return list of new points in the last second
+    if request.method == "POST":
+        data = States.passStateToFrontEnd()
+        data = json.dumps(data)
+        return HttpResponse(data)
+    return HttpResponseRedirect('/')
